6e50460db5079d6c29276d85c4ca87407627b7c0
YanJa <YanJa99@163.com>
2020-09-22 10:55:10 +0800

    Update s3_test.py

--- mysql/backuptools/s3_test.py
+++ mysql/backuptools/s3_test.py
@@ -2,8 +2,8 @@
 from boto.s3.connection import S3Connection
 from boto.s3.key import Key
 
-AWS_ACCESS_KEY_ID = 'AKIAITBLHJCXJFY4CXEQ'
-AWS_SECRET_ACCESS_KEY = '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'
+AWS_ACCESS_KEY_ID = ''
+AWS_SECRET_ACCESS_KEY = ''
 c = S3Connection(AWS_ACCESS_KEY_ID,AWS_SECRET_ACCESS_KEY)
 
 b = c.get_bucket('fzx')
