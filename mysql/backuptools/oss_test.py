f3070f55911623c16b8c9f658565228b89b8faa7
YanJa <YanJa99@163.com>
2020-09-22 10:54:46 +0800

    Update oss_test.py

--- mysql/backuptools/oss_test.py
+++ mysql/backuptools/oss_test.py
@@ -3,7 +3,7 @@ import os
 from oss.oss_api import OssAPI
 
 BUCKET_NAME = "fzx-backup"
-oss = OssAPI("oss.aliyuncs.com", "RPFrp3cHPqEYWdUC", "jaDVkNHMvJpGSfXV9euC7AR4uTyI3k")
+oss = OssAPI("oss.aliyuncs.com", "", "")
 
 part = 'crontab.txt'
 part_filename = './crontab.txt'
